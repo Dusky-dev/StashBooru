05338af97fa2ef9d8804f01734d37e4731388fb6
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-11 16:26:37 +0300

    Hide Copyright namespace from general Tags

--- ui/v2.5/src/components/Tags/Tags.tsx
+++ ui/v2.5/src/components/Tags/Tags.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { useTitleProps } from "src/hooks/title";
+import { ListFilterModel } from "src/models/list-filter/filter";
 import Tag from "./TagDetails/Tag";
 import TagCreate from "./TagDetails/TagCreate";
 import { FilteredTagList } from "./TagList";
+import {
+  applyCopyrightNamespaceFilter,
+  CopyrightRoot,
+} from "./copyrightFilter";
 
 const Tags: React.FC = () => {
-  return <FilteredTagList />;
+  const [copyrightRoot, setCopyrightRoot] = useState<CopyrightRoot | null>();
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        const response = await fetch(
+          "image/visual-similarity/camie/copyright-root"
+        );
+        if (!response.ok) throw new Error(await response.text());
+        setCopyrightRoot((await response.json()) as CopyrightRoot);
+      } catch {
+        setCopyrightRoot(null);
+      }
+    })();
+  }, []);
+
+  const filterHook = useCallback(
+    (filter: ListFilterModel) =>
+      copyrightRoot
+        ? applyCopyrightNamespaceFilter(filter, copyrightRoot, false)
+        : filter,
+    [copyrightRoot]
+  );
+
+  if (copyrightRoot === undefined) return null;
+  return <FilteredTagList filterHook={filterHook} />;
 };
 
 const TagRoutes: React.FC = () => {
