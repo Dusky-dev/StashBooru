2dcbeed8c035e8c2e3b9e51523ed32ffd87b5f2b
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-11 16:25:23 +0300

    Expose Copyright namespace metadata

--- internal/api/routes_image.go
+++ internal/api/routes_image.go
@@ -44,6 +44,7 @@ func (rs imageRoutes) Routes() chi.Router {
 		r.Get("/camie/status", rs.CamieStatus)
 		r.Get("/camie/config", rs.CamieConfig)
 		r.Post("/camie/config", rs.CamieConfigUpdate)
+		r.Get("/camie/copyright-root", rs.CamieCopyrightRoot)
 		r.Post("/camie/tag", rs.CamieTagImagesV2)
 	})
 
