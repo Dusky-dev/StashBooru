f53de0328117836d3f6362c3803e0366d49d5f48
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-12 23:02:20 +0300

    Remove duplicate image metadata cards

--- ui/v2.5/src/components/Images/ImageDetails/ImageDetailPanel.tsx
+++ ui/v2.5/src/components/Images/ImageDetails/ImageDetailPanel.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import * as GQL from "src/core/generated-graphql";
 import TextUtils from "src/utils/text";
-import { GalleryLink, TagLink } from "src/components/Shared/TagLink";
-import { CopyrightLink } from "src/components/Copyrights/CopyrightLink";
-import { PerformerCard } from "src/components/Performers/PerformerCard";
-import { sortPerformers } from "src/core/performers";
+import { GalleryLink } from "src/components/Shared/TagLink";
 import { FormattedMessage, useIntl } from "react-intl";
 import { PhotographerLink } from "src/components/Shared/Link";
 import { PatchComponent } from "../../../patch";
@@ -32,70 +29,6 @@ export const ImageDetailPanel: React.FC<IImageDetailProps> = PatchComponent(
       );
     }
 
-    function renderMetadata() {
-      const copyrights = props.image.copyrights ?? [];
-      const tags = props.image.tags;
-      if (tags.length === 0 && copyrights.length === 0) return;
-
-      return (
-        <>
-          {copyrights.length > 0 && (
-            <>
-              <h6>
-                {intl.formatMessage({
-                  id: "copyrights",
-                  defaultMessage: "Copyrights",
-                })}{" "}
-                ({copyrights.length})
-              </h6>
-              {copyrights.map((copyright) => (
-                <CopyrightLink key={copyright.id} copyright={copyright} />
-              ))}
-            </>
-          )}
-          {tags.length > 0 && (
-            <>
-              <h6>
-                <FormattedMessage
-                  id="countables.tags"
-                  values={{ count: tags.length }}
-                />
-              </h6>
-              {tags.map((tag) => (
-                <TagLink key={tag.id} tag={tag} linkType="image" />
-              ))}
-            </>
-          )}
-        </>
-      );
-    }
-
-    function renderPerformers() {
-      if (props.image.performers.length === 0) return;
-      const performers = sortPerformers(props.image.performers);
-      const cards = performers.map((performer) => (
-        <PerformerCard
-          key={performer.id}
-          performer={performer}
-          ageFromDate={props.image.date ?? undefined}
-        />
-      ));
-
-      return (
-        <>
-          <h6>
-            <FormattedMessage
-              id="countables.performers"
-              values={{ count: props.image.performers.length }}
-            />
-          </h6>
-          <div className="row justify-content-center image-performers">
-            {cards}
-          </div>
-        </>
-      );
-    }
-
     function renderGalleries() {
       if (props.image.galleries.length === 0) return;
       const galleries = props.image.galleries.map((gallery) => (
@@ -114,27 +47,19 @@ export const ImageDetailPanel: React.FC<IImageDetailProps> = PatchComponent(
       );
     }
 
-    // filename should use entire row if there is no studio
-    const imageDetailsWidth = props.image.studio ? "col-9" : "col-12";
-
     return (
       <>
         <div className="row">
-          <div className={`${imageDetailsWidth} col-12 image-details`}>
+          <div className="col-12 image-details">
             {renderGalleries()}
-            {
-              <h6>
-                {" "}
-                <FormattedMessage id="created_at" />:{" "}
-                {TextUtils.formatDateTime(intl, props.image.created_at)}{" "}
-              </h6>
-            }
-            {
-              <h6>
-                <FormattedMessage id="updated_at" />:{" "}
-                {TextUtils.formatDateTime(intl, props.image.updated_at)}{" "}
-              </h6>
-            }
+            <h6>
+              <FormattedMessage id="created_at" />:{" "}
+              {TextUtils.formatDateTime(intl, props.image.created_at)}{" "}
+            </h6>
+            <h6>
+              <FormattedMessage id="updated_at" />:{" "}
+              {TextUtils.formatDateTime(intl, props.image.updated_at)}{" "}
+            </h6>
             {props.image.code && (
               <h6>
                 <FormattedMessage id="scene_code" />: {props.image.code}{" "}
@@ -154,8 +79,6 @@ export const ImageDetailPanel: React.FC<IImageDetailProps> = PatchComponent(
         <div className="row">
           <div className="col-12">
             {renderDetails()}
-            {renderMetadata()}
-            {renderPerformers()}
             <CustomFields values={props.image.custom_fields} fullWidth />
             <hr />
             <h6>File info</h6>
