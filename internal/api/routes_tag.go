89d1ecf5f619cb384893dba2061cbf4fd7e51e31
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-12 10:16:17 +0300

    Serve native Copyright images

--- internal/api/routes_tag.go
+++ internal/api/routes_tag.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/stashapp/stash/internal/manager"
 	"github.com/stashapp/stash/internal/static"
 	"github.com/stashapp/stash/pkg/logger"
 	"github.com/stashapp/stash/pkg/models"
@@ -27,12 +28,46 @@ type tagRoutes struct {
 func (rs tagRoutes) Routes() chi.Router {
 	r := chi.NewRouter()
 
+	// Copyright is a separate metadata category, but this lightweight image
+	// endpoint shares the already-mounted metadata-image router.
+	r.Get("/copyright/{copyrightId}/image", rs.CopyrightImage)
+
 	r.Route("/{tagId}", func(r chi.Router) {
 		r.Use(rs.TagCtx)
 		r.Get("/image", rs.Image)
 	})
 
 	return r
+}
+
+func (rs tagRoutes) CopyrightImage(w http.ResponseWriter, r *http.Request) {
+	copyrightID, err := strconv.Atoi(chi.URLParam(r, "copyrightId"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	defaultParam := r.URL.Query().Get("default")
+	var image []byte
+	if defaultParam != "true" {
+		repository := manager.GetInstance().Repository
+		readTxnErr := rs.withReadTxn(r, func(ctx context.Context) error {
+			var err error
+			image, err = repository.Copyright.GetImage(ctx, copyrightID)
+			return err
+		})
+		if errors.Is(readTxnErr, context.Canceled) {
+			return
+		}
+		if readTxnErr != nil {
+			logger.Warnf("read transaction error on fetch copyright image: %v", readTxnErr)
+		}
+	}
+
+	if len(image) == 0 {
+		image = static.ReadAll(static.DefaultTagImage)
+	}
+	utils.ServeImage(w, r, image)
 }
 
 func (rs tagRoutes) Image(w http.ResponseWriter, r *http.Request) {
