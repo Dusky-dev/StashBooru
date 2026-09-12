545942ee86003ec7fc9a8b9480719a39459c17d8
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-12 10:10:26 +0300

    Add Copyright image and Character count fields

--- pkg/models/model_copyright.go
+++ pkg/models/model_copyright.go
@@ -5,18 +5,19 @@ import "time"
 // Copyright is a first-class series/franchise metadata entity. It deliberately
 // does not share Tag storage or require a synthetic root Tag.
 type Copyright struct {
-	ID          int          `json:"id"`
-	Name        string       `json:"name"`
-	SortName    string       `json:"sort_name"`
-	Description string       `json:"description"`
-	Favorite    bool         `json:"favorite"`
-	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updated_at"`
-	Aliases     []string     `json:"aliases"`
-	Parents     []*Copyright `json:"parents"`
-	Children    []*Copyright `json:"children"`
-	ImageCount  int          `json:"image_count"`
-	SceneCount  int          `json:"scene_count"`
+	ID             int          `json:"id"`
+	Name           string       `json:"name"`
+	SortName       string       `json:"sort_name"`
+	Description    string       `json:"description"`
+	Favorite       bool         `json:"favorite"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
+	Aliases        []string     `json:"aliases"`
+	Parents        []*Copyright `json:"parents"`
+	Children       []*Copyright `json:"children"`
+	ImageCount     int          `json:"image_count"`
+	SceneCount     int          `json:"scene_count"`
+	PerformerCount int          `json:"performer_count"`
 }
 
 type CopyrightCreateInput struct {
@@ -27,6 +28,8 @@ type CopyrightCreateInput struct {
 	Aliases     []string `json:"aliases"`
 	ParentIDs   []string `json:"parent_ids"`
 	ChildIDs    []string `json:"child_ids"`
+	// This should be a URL or a base64 encoded data URL.
+	Image *string `json:"image"`
 }
 
 type CopyrightUpdateInput struct {
@@ -38,6 +41,8 @@ type CopyrightUpdateInput struct {
 	Aliases     []string `json:"aliases"`
 	ParentIDs   []string `json:"parent_ids"`
 	ChildIDs    []string `json:"child_ids"`
+	// This should be a URL or a base64 encoded data URL.
+	Image *string `json:"image"`
 }
 
 type CopyrightDestroyInput struct {
