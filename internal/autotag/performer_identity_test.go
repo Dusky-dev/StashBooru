47d1f13fd7e4e666ef42f4be18944de40a681558
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-22 22:52:21 +0300

    test: cover specific performer autotag identities

---
+++ internal/autotag/performer_identity_test.go
@@ -0,0 +1,32 @@
+package autotag
+
+import (
+	"testing"
+
+	"github.com/stashapp/stash/pkg/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPerformerTaggersUseDisambiguationAndExplicitAliases(t *testing.T) {
+	t.Parallel()
+
+	performer := &models.Performer{
+		ID:             1,
+		Name:           "Echidna",
+		Disambiguation: "Re:Zero",
+		Aliases:        models.NewRelatedStrings([]string{"Knuckles the Echidna"}),
+	}
+
+	taggers := getPerformerTaggers(performer, nil)
+	if assert.Len(t, taggers, 2) {
+		canonical := taggers[0]
+		alias := taggers[1]
+
+		assert.Equal(t, "Echidna", canonical.Name)
+		assert.True(t, canonical.matchesPath("/anime/Echidna - ReZero/image.jpg"))
+		assert.False(t, canonical.matchesPath("/sonic/Knuckles the Echidna/image.jpg"))
+
+		assert.Equal(t, "Knuckles the Echidna", alias.Name)
+		assert.True(t, alias.matchesPath("/sonic/Knuckles the Echidna/image.jpg"))
+	}
+}
