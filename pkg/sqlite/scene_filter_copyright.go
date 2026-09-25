b0a5f5e366506f1aedc9f77e8f43d6eb2ca29cf5
Dusky <76793489+Dusky-dev@users.noreply.github.com>
2026-09-25 14:49:29 +0300

    gofmt Copyright video filter handler

--- pkg/sqlite/scene_filter_copyright.go
+++ pkg/sqlite/scene_filter_copyright.go
@@ -4,13 +4,13 @@ import "github.com/stashapp/stash/pkg/models"
 
 func (qb *sceneFilterHandler) copyrightsCriterionHandler(copyrights *models.HierarchicalMultiCriterionInput) criterionHandlerFunc {
 	h := joinedHierarchicalMultiCriterionHandlerBuilder{
-		primaryTable:    sceneTable,
-		foreignTable:    copyrightTable,
-		foreignFK:       "copyright_id",
+		primaryTable:   sceneTable,
+		foreignTable:   copyrightTable,
+		foreignFK:      "copyright_id",
 		relationsTable: copyrightRelationsTable,
-		joinAs:          "copyrights_join",
-		joinTable:       scenesCopyrightsTable,
-		primaryFK:       sceneIDColumn,
+		joinAs:         "copyrights_join",
+		joinTable:      scenesCopyrightsTable,
+		primaryFK:      sceneIDColumn,
 	}
 
 	return h.handler(copyrights)
